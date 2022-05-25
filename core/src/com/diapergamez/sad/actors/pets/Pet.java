7d9f8e9022ef9b093b71d46ba6ac335dcefb0c42
breadchrom <kkchrom@gmail.com>
2022-05-25 12:34:09 -0700

    i love programming ^w^ nyaa~~~ :3

--- core/src/com/diapergamez/sad/actors/pets/Pet.java
+++ core/src/com/diapergamez/sad/actors/pets/Pet.java
@@ -1,5 +1,7 @@
 package com.diapergamez.sad.actors.pets;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -8,16 +10,23 @@ import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 
 
 public class Pet extends Actor {
-    Sprite sprite;
-
-    public Pet()
+    public Sprite sprite;
+    public int tw, th;
+    Texture picture;
+    public Pet(float x, float y)
     {
         setBounds(sprite.getX(),sprite.getY(),sprite.getWidth(),sprite.getHeight());
         setTouchable(Touchable.enabled);
         setVisible(false);
 
         }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
     }
+}
+
 //TODO implement rest of pet constructor(maybe make action listener) and add the rest of parameters like video :D
 //https://www.youtube.com/watch?v=A_-rxTO5138
 
