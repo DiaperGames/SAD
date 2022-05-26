dc3dab2760ecee14a7cd1691bbb931dae667e080
liam <liam.wirth1@outlook.com>
2022-05-26 09:08:35 -0700

    did a lil work, and just implemented the pet class, no big deal 😎😎😎

--- core/src/com/diapergamez/sad/actors/pets/Pet.java
+++ core/src/com/diapergamez/sad/actors/pets/Pet.java
@@ -1,33 +1,150 @@
 package com.diapergamez.sad.actors.pets;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.diapergamez.sad.Items;
 
+import java.lang.reflect.Array;
 
 public class Pet extends Actor {
-    public Sprite sprite;
     public int tw, th;
-    Texture picture;
-    public Pet(float x, float y)
-    {
-        setBounds(sprite.getX(),sprite.getY(),sprite.getWidth(),sprite.getHeight());
+    private Texture petPic;
+    private ParticleEffect itemEffect;
+    private byte health,attack; //0-50 range
+    private boolean hasHouse, hasMaxVal, hasParallel, hasPi, hasSemicolon, hasSwitch, hasTicTacToe;
+    private byte turn;
+    public TextureAtlas items;
+    private ParticleEmitter particleEffects[];
+    //below bool is for the maxValue item
+    //TODO We'd likely have to do stat checking of the units at the start of each turn
+    private boolean isStrongest;
+    private byte level;
+    /**
+    * X & Y, coordinates as to where you want texture to be drawn
+    * (is drawn from bottom left)
+    * Implement texture parameter in subclass, push texture parameter to super.
+     */
+    public Pet(float x, float y, Texture texture) {
+        petPic = texture;
+        setBounds(x,y, petPic.getWidth(), petPic.getHeight());
         setTouchable(Touchable.enabled);
         setVisible(false);
 
         }
 
+    /*
+     this is probably more efficient than building a bunch of item classes.
+     TODO: depending on how we implement turns in the game, we'll refactor/format our code
+         here to reflect what turn it is, my best bet, is to constantly update the pets
+         with what turn it is in the stages render method, which get's called each frame.
+    */
+    /**
+    *
+    *
+    *
+    *
+    * ONLY CALL MAXVALUE IF This.isStrongest= true
+    *
+     */
+    public void giveItem(Items item){
+            switch (item) {
+                case HOUSE:
+                    health+=2;
+                    //loading the particle file, and it's associated item
+
+                  if(!hasHouse) {
+                      particleEffects[0] = itemEffect.findEmitter("House");
+                      hasHouse = true;
+                  }
+                    break;
+                case MAXVALUE:
+                   //TODO someone else implement maxvalue in a memory conscious way
+                    break;
+                case PARALELL:
+                    if(!hasParallel){
+
+                        hasParallel = true;
+                    }
+                    break;
+                case PI:
+                    if(!hasPi){
+                        hasPi = true;
+                    }
+                    break;
+                case SEMICOLON:
+                    if(!hasSemicolon){
+                        hasSemicolon = true;
+                    }
+                    break;
+                case SWITCH:
+                    if(!hasSwitch){
+                       hasSwitch = true;
+                    }
+                    break;
+                case TICTACTOE:
+                    if(!hasTicTacToe){
+                        hasTicTacToe = true;
+                    }
+                    break;
+            }
+
+        }
+
+
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
     }
+
+
+
+
+
+
+
+
+
+
+    public byte getHealth() {
+        return health;
+    }
+
+    public void setHealth(byte health) {
+        this.health = health;
+    }
+
+    public byte getAttack() {
+        return attack;
+    }
+
+    public void setAttack(byte attack) {
+        this.attack = attack;
+    }
+
+    public byte getTurn() {
+        return turn;
+    }
+
+    public void setTurn(byte turn) {
+        this.turn = turn;
+    }
+
+    public boolean isStrongest() {
+        return isStrongest;
+    }
+
+    public void setStrongest(boolean strongest) {
+        isStrongest = strongest;
+    }
+
+    public byte getLevel() {
+        return level;
+    }
+
+    public void setLevel(byte level) {
+        this.level = level;
+    }
 }
-
-//TODO implement rest of pet constructor(maybe make action listener) and add the rest of parameters like video :D
-//https://www.youtube.com/watch?v=A_-rxTO5138
-
-
